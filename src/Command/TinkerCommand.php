20d7710f2c892bbad80174d3a74f55f30d62b2f5
Boris Bobol <bobol.boris@gmail.com>
2022-03-25 16:36:00 +0300

    25.03.2021 16:35

--- src/Command/TinkerCommand.php
+++ src/Command/TinkerCommand.php
@@ -3,16 +3,23 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
+use App\Domain\Common\Application\CommandBus\CommandBus;
+use App\Domain\Graber\Application\Command\ParseProductListAll;
 
 class TinkerCommand extends Command
 {
     protected static $defaultName = 'tinker';
 
-    public function __construct()
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
     {
         parent::__construct();
+        $this->commandBus = $commandBus;
     }
 
     /**
@@ -22,6 +29,29 @@ class TinkerCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        /** @var ConsoleOutput $output */
+
+        $categoryProgressBar = new ProgressBar($output->section());
+        $categoryProgressBar->setFormat('%current%/%max% [%bar%] %percent% %elapsed%');
+        $categoryProgressBar->start();
+
+        $productProgressBar = new ProgressBar($output->section());
+        $productProgressBar->setFormat('%current%/%max% [%bar%] %percent% %memory%');
+        $productProgressBar->start();
+
+        $command = new ParseProductListAll();
+        $command->setOnHandledProduct(static fn() => $productProgressBar->advance());
+        $command->setOnHandledCategory(static fn() => $categoryProgressBar->advance());
+        $command->setOnReceivedProductList(static fn(int $number) => $productProgressBar->start($number));
+        $command->setOnReceivedCategoryList(static fn(int $number) => $categoryProgressBar->start($number));
+
+        $this->commandBus->execute($command);
+
+        $productProgressBar->finish();
+        $categoryProgressBar->finish();
+
+        $output->write(PHP_EOL);
+
         return self::SUCCESS;
     }
 }
